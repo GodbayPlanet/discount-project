f9ec68cec80a36b94fcf12344fef890d92218855
Nemanja Vasic <nemanjavasa@gmail.com>
2017-11-19 22:28:52 +0100

    First Commit

---
+++ src/main/java/com/discount/DiscountProjectApplication.java
@@ -0,0 +1,12 @@
+package com.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountProjectApplication.class, args);
+	}
+}
