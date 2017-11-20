e7abf9a0c7f16e73028256d3b699a8a72a39978b
Nemanja Vasic <nemanjavasa@gmail.com>
2017-11-20 12:01:31 +0100

    Added new method for creating RestTemplate Bean

--- src/main/java/com/discount/service/UserServiceImpl.java
+++ src/main/java/com/discount/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.discount.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,13 +12,29 @@ import com.discount.wrappers.UserWrapper;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	public UserServiceImpl() {
+		this.restTemplate = restTemplate();
+	}
 
+	/**
+	 * This method returns list of users from server where all user data exists.
+	 */
 	@Override
 	public List<User> listOfUsers() {
-		RestTemplate restTemplate = new RestTemplate();
 		UserWrapper userWrapper = restTemplate.getForObject("http://localhost:8000/api/users/", UserWrapper.class);
-		
 		return userWrapper.getUsers();
 	}
 
+	/**
+	 * Defining RestTemplate Bean.
+	 * @return
+	 */
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
 }
