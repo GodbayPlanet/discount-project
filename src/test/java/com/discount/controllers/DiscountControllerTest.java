2dab6ddb574d261ecb05a30654868fdd7a399fe7
Nemanja Vasic <nemanjavasa@gmail.com>
2017-11-21 19:53:32 +0100

    Added test case for DiscountController

---
+++ src/test/java/com/discount/controllers/DiscountControllerTest.java
@@ -0,0 +1,54 @@
+package com.discount.controllers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.discount.DiscountProjectApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DiscountProjectApplication.class)
+public class DiscountControllerTest {
+
+	@InjectMocks
+	private DiscountController discountController;
+
+	@Autowired
+	private WebApplicationContext context;
+	
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		mvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+
+	/**
+	 * Test method for getting users from http://localhost:8080/api/users/ path.
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetUsers() throws Exception {
+		mvc.perform(get("/api/users/"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$", hasSize(10)));
+		
+	}
+}
