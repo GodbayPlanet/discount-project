c57954378d4aa43369f77efb9c08daddc8635297
Nemanja Vasic <nemanjavasa@gmail.com>
2017-11-22 19:50:54 +0100

    Added ProductByProductIdWrapper class

---
+++ src/main/java/com/discount/wrappers/ProductByProductIdWrapper.java
@@ -0,0 +1,16 @@
+package com.discount.wrappers;
+
+import com.discount.domain.Product;
+
+public class ProductByProductIdWrapper {
+
+	private Product product;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
