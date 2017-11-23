5db9c6258ca8ede36b34c837c30df3a875d40c6c
Nemanja Vasic <nemanjavasa@gmail.com>
2017-11-23 18:28:43 +0100

    Modified getListOfPurchasesByUser in PurchaseController class

--- src/main/java/com/discount/controllers/PurchaseController.java
+++ src/main/java/com/discount/controllers/PurchaseController.java
@@ -1,10 +1,13 @@
 package com.discount.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.discount.domain.Purchase;
@@ -13,6 +16,7 @@ import com.discount.service.PurchaseService;
 @RestController
 public class PurchaseController {
 
+	private static final int FROM_INDEX = 0;
 	private PurchaseService purchaseService;
 	
 	@Autowired
@@ -22,12 +26,22 @@ public class PurchaseController {
 	
 	/**
 	 * Method use PurchaseService to return list of purchases by specific user based on given userName.
+	 * 
 	 * @param userName
 	 * @return
 	 */
 	@RequestMapping("/api/purchases/by_user/{userName}")
-	public List<Purchase> getListOfPurchasesByUser(@PathVariable("userName") String userName) {
-		return purchaseService.listOfPurchasesByUser(userName);
+	public List<Purchase> getListOfPurchasesByUser(@PathVariable("userName") String userName, 
+			@RequestParam(value = "limit", required = false) Integer limit) {
+		
+		List<Purchase> listOfPurchasesByUser = purchaseService.listOfPurchasesByUser(userName);
+		
+		if (limit == null) {
+			return listOfPurchasesByUser;
+		} else {
+			List<Purchase> newList = listOfPurchasesByUser.stream().collect(Collectors.toList()).subList(FROM_INDEX, limit);
+			return newList;
+		}
 	}
 	
 }
